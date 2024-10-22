c19f318ffb9e6aade55c62a6772a5ec62a1558c1
Enith Linares <enithalinares@gmail.com>
2024-10-21 23:33:01 -0400

    create header component, update resets to include font family

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo/date-for-1.svg";
 
 export default function Header() {
     return (
-        <div>
-            <p>
-                <Link to="/quiz">Take the Quiz</Link>
-            </p>
-            <p>
-                <Link to="/activities">Browse Activities</Link>
-            </p>
-        </div>
+        <nav className="nav">
+            <img src={logo} alt="date for 1 logo" className="nav__logo" />
+            <div className="nav__buttons">
+                <button className="options">
+                    <Link to="/quiz">Take the Quiz</Link>
+                </button>
+
+                <button className="options">
+                    <Link to="/activities">Browse Activities</Link>
+                </button>
+            </div>
+        </nav>
     );
 }
