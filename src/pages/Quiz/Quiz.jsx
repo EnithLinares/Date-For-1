d68ad674d09a51bdca92610b2ff3e9cddb39dd86
Enith Linares <enithalinares@gmail.com>
2024-10-26 19:47:12 -0400

    finish logic for quiz, include message for when no acttivity fits the filters applied during the quiz

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./Quiz.scss";
 import { useNavigate } from "react-router-dom";
 
 function Quiz() {
@@ -43,44 +44,40 @@ function Quiz() {
                 <div>
                     <h3>How do you feel today?</h3>
                     <div
-                        className="card"
-                        onClick={() => handleSelect("mood", "adventurous")}
+                        className={`card ${
+                            answers.mood === "Creative" ? "selected" : ""
+                        }`}
+                        onClick={() => handleSelect("mood", "Creative")}
                     >
-                        <img
-                            src="/images/adventure.jpg"
-                            alt="a guys legs hanging on top of a mountain landscape"
-                        />
-                        <p>I feel Adventurous!</p>
-                    </div>
-                    <div
-                        className="card"
-                        onClick={() => handleSelect("mood", "hungry")}
-                    >
-                        <img
-                            src="/images/hungry.jpg"
-                            alt="a delicious looking bowl of rice and prawns with curry"
-                        />
-                        <p>I'm feeling Hungry</p>
-                    </div>
-                    <div
-                        className="card"
-                        onClick={() => handleSelect("mood", "creative")}
-                    >
-                        <img
-                            src="/images/creative.jpg"
-                            alt="notepad with watercolors and brushes"
-                        />
+                        <img src="/images/creative.jpg" alt="Creative" />
                         <p>I'm feeling Creative</p>
                     </div>
                     <div
-                        className="card"
-                        onClick={() => handleSelect("mood", "cozy")}
+                        className={`card ${
+                            answers.mood === "Adventurous" ? "selected" : ""
+                        }`}
+                        onClick={() => handleSelect("mood", "Adventurous")}
                     >
-                        <img
-                            src="/images/cozy.jpg"
-                            alt="a reading nooks surrounded by bookshelves"
-                        />
+                        <img src="/images/adventurous.jpg" alt="Adventurous" />
+                        <p>I feel Adventurous!</p>
+                    </div>
+                    <div
+                        className={`card ${
+                            answers.mood === "Cozy" ? "selected" : ""
+                        }`}
+                        onClick={() => handleSelect("mood", "Cozy")}
+                    >
+                        <img src="/images/cozy.jpg" alt="Cozy" />
                         <p>I'm feeling Cozy</p>
+                    </div>
+                    <div
+                        className={`card ${
+                            answers.mood === "Hungry" ? "selected" : ""
+                        }`}
+                        onClick={() => handleSelect("mood", "Hungry")}
+                    >
+                        <img src="/images/hungry.jpg" alt="Hungry" />
+                        <p>I'm feeling Hungry</p>
                     </div>
                 </div>
             )}
@@ -88,18 +85,65 @@ function Quiz() {
                 <div>
                     <h3>At what time will you like to go?</h3>
                     <div
-                        className="card"
-                        onClick={() => handleSelect("timeOfDay", "morning")}
+                        className={`card ${
+                            answers.timeOfDay === "Early Bird (7 am to 9 am)"
+                                ? "selected"
+                                : ""
+                        }`}
+                        onClick={() =>
+                            handleSelect(
+                                "timeOfDay",
+                                "Early Bird (7 am to 9 am)"
+                            )
+                        }
                     >
-                        <img src="/images/morning.jpg" alt="Morning" />
-                        <p>Early bird gets the worm (7am - 10am)</p>
+                        <img src="/images/early-morning.jpg" alt="Morning" />
+                        <p>Early bird gets the worm (7 am - 9 am)</p>
                     </div>
                     <div
-                        className="card"
-                        onClick={() => handleSelect("timeOfDay", "evening")}
+                        className={`card ${
+                            answers.timeOfDay === "Anytime after 10 am"
+                                ? "selected"
+                                : ""
+                        }`}
+                        onClick={() =>
+                            handleSelect("timeOfDay", "Anytime after 10 am")
+                        }
                     >
-                        <img src="/images/evening.jpg" alt="Evening" />
-                        <p>Anytime after 10am</p>
+                        <img src="/images/10am.jpg" alt="mid-afternoon" />
+                        <p>Anytime after 10 am</p>
+                    </div>
+                    <div
+                        className={`card ${
+                            answers.timeOfDay === "After work outing (5 pm+)"
+                                ? "selected"
+                                : ""
+                        }`}
+                        onClick={() =>
+                            handleSelect(
+                                "timeOfDay",
+                                "After work outing (5 pm+)"
+                            )
+                        }
+                    >
+                        <img src="/images/after-work.jpg" alt="Evening" />
+                        <p>I have to work (after 5 pm)</p>
+                    </div>
+                    <div
+                        className={`card ${
+                            answers.timeOfDay === "Night owl (9 pm to 2 am)"
+                                ? "selected"
+                                : ""
+                        }`}
+                        onClick={() =>
+                            handleSelect(
+                                "timeOfDay",
+                                "Night owl (9 pm to 2 am)"
+                            )
+                        }
+                    >
+                        <img src="/images/night-owl.jpg" alt="Evening" />
+                        <p>I'm a night owl</p>
                     </div>
                 </div>
             )}
@@ -107,21 +151,65 @@ function Quiz() {
                 <div>
                     <h3>What is your budget for this date?</h3>
                     <div
-                        className="card"
-                        onClick={() => handleSelect("priceRange", "low")}
+                        className={`card ${
+                            answers.priceRange === "$ (Free to $20)"
+                                ? "selected"
+                                : ""
+                        }`}
+                        onClick={() =>
+                            handleSelect("priceRange", "$ (Free to $20)")
+                        }
                     >
-                        <img src="/images/low-budget.jpg" alt="Low Budget" />
+                        <img src="/images/budget.jpg" alt="Low Budget" />
                         <p>I'm on a budget (free - $20)</p>
                     </div>
                     <div
-                        className="card"
-                        onClick={() => handleSelect("priceRange", "medium")}
+                        className={`card ${
+                            answers.priceRange === "$$ ($21 to $59)"
+                                ? "selected"
+                                : ""
+                        }`}
+                        onClick={() =>
+                            handleSelect("priceRange", "$$ ($21 to $59)")
+                        }
                     >
                         <img
-                            src="/images/medium-budget.jpg"
+                            src="/images/little-treat.jpg"
                             alt="Medium Budget"
                         />
-                        <p>I deserve a little treat ($21 - $60)</p>
+                        <p>I deserve a little treat ($21 - $59)</p>
+                    </div>
+                    <div
+                        className={`card ${
+                            answers.priceRange === "$$$ ($60 to $150)"
+                                ? "selected"
+                                : ""
+                        }`}
+                        onClick={() =>
+                            handleSelect("priceRange", "$$$ ($60 to $150)")
+                        }
+                    >
+                        <img
+                            src="/images/special.jpg"
+                            alt="sparkling drinks on a ledge"
+                        />
+                        <p>I want to make it special ($60 - $150)</p>
+                    </div>
+                    <div
+                        className={`card ${
+                            answers.priceRange === "$$$$ ($150+)"
+                                ? "selected"
+                                : ""
+                        }`}
+                        onClick={() =>
+                            handleSelect("priceRange", "$$$$ ($150+)")
+                        }
+                    >
+                        <img
+                            src="/images/expensive.jpg"
+                            alt="expensive necklace"
+                        />
+                        <p>I'm going all out ($150+)</p>
                     </div>
                 </div>
             )}
--- src/pages/Recommendation/Recommendations.jsx
+++ src/pages/Recommendation/Recommendations.jsx
@@ -9,16 +9,24 @@ function Recommendations() {
     return (
         <div>
             <h2>Recommended Activities</h2>
-            {activities.map((activity) => (
-                <div key={activity.id} className="activity-card">
-                    <h3>{activity.name}</h3>
-                    {/* Add more details and links to activity details if needed */}
+            {activities.message ? (
+                <div>
+                    <p>{activities.message}</p>
+                    <button onClick={() => navigate("/activities")}>
+                        View All Activities
+                    </button>
+                    <button onClick={() => navigate("/quiz")}>
+                        Take Quiz Again
+                    </button>
                 </div>
-            ))}
-            <button onClick={() => navigate("/activities")}>
-                All Activities
-            </button>
-            <button onClick={() => navigate("/quiz")}>Take Quiz Again</button>
+            ) : (
+                activities.map((activity) => (
+                    <div key={activity.id} className="activity-card">
+                        <h3>{activity.name}</h3>
+                        {/* Add more details and links to activity details if needed */}
+                    </div>
+                ))
+            )}
         </div>
     );
 }
